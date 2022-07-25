b537f21db3bb8b7eb580e317dc5cccc3e1a5ae76
Elia Ashraf <eliaashrafw@gmail.com>
2022-07-25 16:17:55 +0200

    First commit

---
+++ Separate_rows.py
@@ -0,0 +1,26 @@
+from pickle import FALSE
+import pandas as pd
+#import csv 
+#import datetime as dt
+
+#Read from the csv file and parse out the timestamp
+Data = pd.read_csv('C:\\Users\\Elia Ashraf\\OneDrive\\Desktop\\conversations\\chatbot_convo_FR.csv', parse_dates=['Time_stamp'], infer_datetime_format=True)
+
+#Parse out the date from the ISO timestamp and change the format from YYYY-MM-DD to DD-MM-YYYY
+Data['Date'] = pd.to_datetime(Data['Time_stamp']).dt.strftime('%d/%m/%Y')
+
+#Parse out the time from the ISO time format and drop everything except HH:MM:SS
+Data['Time'] = pd.to_datetime(Data['Time_stamp']).dt.strftime('%H:%M:%S')
+#print (Data["Time"])
+
+#Write to a new file (save changes to a new file)
+Data.to_csv('C:\\Users\\Elia Ashraf\\OneDrive\\Desktop\\conversations\\Chatbot_UserStats_FR.csv', index=FALSE)
+
+
+
+##timestamps = Data.Time_Stamp
+##for i in timestamps:
+    ##dataobj = datetime.strptime(i, "%d/%m/%Y %H:%M")
+
+##dt['new_date'] = [i.date() for i in Data['Time_Stamp']]
+##dt['new_time'] = [i.time() for i in Data['Time_Stamp']]
